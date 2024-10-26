abe8280460165a81d1a2a54cca0e959670868ea9
María V <mariatua2@gmail.com>
2024-10-25 21:51:24 -0400

    Initial Commit

---
+++ StockTrackerApp/StockTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerAppApp.swift
+//  StockTrackerApp
+//
+//  Created by Maria on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
